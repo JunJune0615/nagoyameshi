8537612595d5a779921a1b4151e81a0bc09c13a0
JunJune0615 <168808114+JunJune0615@users.noreply.github.com>
2024-07-28 21:18:48 +0900

    top.htmlの追加と反映するプログラムの変更

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from nagoyameshi import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.TopView.as_view(), name="top"),
 ]
