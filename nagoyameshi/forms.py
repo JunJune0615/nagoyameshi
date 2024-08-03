ddf34f48592659e1947370b9b53565d24c35b4c8
JunJune0615 <168808114+JunJune0615@users.noreply.github.com>
2024-08-03 15:33:29 +0900

    forms.pyの追加

---
+++ nagoyameshi/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import CustomUser
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "username",
+            "email",
+            "password",
+        )
+
