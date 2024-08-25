c7131f7804e0612d9b320f1e064d76d0f0f00a91
JunJune0615 <168808114+JunJune0615@users.noreply.github.com>
2024-08-25 14:53:52 +0900

    chenge forms

--- nagoyameshi/urls.py
+++ nagoyameshi/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-
 from . import views
 
 urlpatterns = [
     path('account/', include('allauth.urls')), 
-    path('profile/',views.ProfileView.as_view(), name='profile')
+    path('profile/',views.ProfileView.as_view(), name='profile'),
+    path('profile/change/', views.UserChangeView.as_view(), name='profiel_change'),
 ]
--- nagoyameshi/views.py
+++ nagoyameshi/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin #ログインしたら見れる
+from .forms import UserChangeForm
+from django.urls import reverse_lazy
+from .models import CustomUser
 
-
-class ProfileView(TemplateView):
+class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'nagoyameshi/profile.html'
+
+
+
+class UserChangeView(LoginRequiredMixin, UpdateView):
+    template_name = 'nagoyameshi/username_edit.html'
+    form_class = UserChangeForm
+    model = CustomUser
+    success_url = reverse_lazy('profile')
+    
+    def get_object(self, queryset=None):
+        return CustomUser.objects.get(username=self.request.user.username)
