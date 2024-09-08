9af6b15ecb978c3206698375c4f3363506b1919a
JunJune0615 <168808114+JunJune0615@users.noreply.github.com>
2024-09-08 14:32:45 +0900

    検索機能の追加

--- nagoyameshi/forms.py
+++ nagoyameshi/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
-from .models import CustomUser
+from .models import CustomUser, Category
+from django import forms
 
 class UserChangeForm(ModelForm):
     class Meta:
@@ -7,3 +8,24 @@ class UserChangeForm(ModelForm):
         fields = [
             'username'
         ]
+
+
+class RestaurantSearchForm(forms.Form):
+    # カテゴリーフィルター
+    category = forms.ModelChoiceField(
+        label='カテゴリでの絞り込み',
+        required=False,
+        queryset=Category.objects.order_by('category_name'),
+        widget=forms.Select(attrs={'class': 'form'})
+    )
+
+    # レストラン名検索
+    restaurant_name = forms.CharField(
+        label='レストラン名での絞り込み',
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'form',
+                                      'autocomplete': 'off',
+                                      'placeholder': '店舗名',
+                                      })
+    )
+
